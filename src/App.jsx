19db4367cbef407d8ff8cecf1ec0d3a3283b1412
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-01-25 20:50:09 +0530

    feat: dashboard first entry css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Dashboard from "./Pages/dashboard.jsx";
 import Login from "./Pages/login.jsx";
 import Register from "./Pages/register.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
       {/* <Route path="*" element={<NoPage />} /> */}
     </Routes>
     </BrowserRouter>
