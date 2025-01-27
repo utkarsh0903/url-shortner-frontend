27a07a533a6c18a216390adbf8357b2529f5cbbb
Utkarsh Garg <utkarshgarg0903@yahoo.in>
2025-01-27 17:58:42 +0530

    feat: display user links table

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+    <div>
+      {/* <table>
+              <thead>
+                <tr>
+                  <th>
+                    Date <img src={dropdown} alt="Sort Date" />
+                  </th>
+                  <th>Original Link</th>
+                  <th>Short Link</th>
+                  <th>Remarks</th>
+                  <th>Clicks</th>
+                  <th>
+                    Status <img src={dropdown} alt="Sort status" />
+                  </th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userLinks.map((link) => {
+                  return (
+                    <tr>
+                      <td>{link.createdAt}</td>
+                      <td>{link.originalLink}</td>
+                      <td>{link.shortLink}</td>
+                      <td>{link.remarks}</td>
+                      <td>{link.clicks}</td>
+                      <td>{link.expiryDate}</td>
+                      <td><button>Edit</button> <button>Delete</button></td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table> */}
+    </div>
   )
 }
 
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { getUserLinks } from "../services";
+import dropdown from "../assets/Dropdown.png";
+import copy from "../assets/copy-link.png";
+import edit from "../assets/edit-link.png";
+import deleteIcon from "../assets/delete-link.png";
 
 const Links = () => {
   const [userLinks, setUserLinks] = useState([]);
@@ -24,11 +28,54 @@ const Links = () => {
     }
   };
 
+  const createdDate = (createdAt) => {
+    console.log(createdAt);
+    const date = new Date(createdAt);
+    const dateFormat = date.toLocaleString("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false
+    }).replace(',', "")
+    return dateFormat;
+  }
+
   return (
-    <div>
-      {userLinks.map((link) => {
-        return <h2 key={link._id}>{link.shortLink}</h2>;
-      })}
+    <div className="link-container">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Date <img src={dropdown} alt="Sort Date" />
+            </th>
+            <th>Original Link</th>
+            <th>Short Link</th>
+            <th>Remarks</th>
+            <th>Clicks</th>
+            <th>
+              Status <img src={dropdown} alt="Sort status" />
+            </th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userLinks.map((link) => {
+            return (
+              <tr key={link._id}>
+                <td>{createdDate(link.createdAt)}</td>
+                <td>{link.originalLink}</td>
+                <td>{link.shortLink} <img src={copy} alt="Copy Icon" /></td>
+                <td>{link.remarks}</td>
+                <td>{link.clicks}</td>
+                <td>{link.expiryDate}</td>
+                <td><img src={edit} alt="Edit Icon" /> <img src={deleteIcon} alt="Delete Icon" /></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
